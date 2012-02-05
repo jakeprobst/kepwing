aa2b1651039a45b80d9dafd2e642a204b92dd0e1
Jake Probst <jake.probst@gmail.com>
2012-02-04 23:48:48 -0800

    control.py: use mecab to deconjugate verbs

--- control.py
+++ control.py
@@ -4,12 +4,18 @@
 from sys import argv
 import os
 import stat
+import MeCab
+
 
 def main(t):
     cb = os.popen("xsel").read()
     path = os.getenv("XDG_CONFIG_HOME") + "/kepwing/control"
     if not stat.S_ISFIFO(os.stat(path)[stat.ST_MODE]):
         return
+
+    m = MeCab.Tagger()
+    cb = m.parseToNode(cb).next.feature.split(',')[6]
+    print cb
 
     f = open(path, "w")
     if t == "popup":
